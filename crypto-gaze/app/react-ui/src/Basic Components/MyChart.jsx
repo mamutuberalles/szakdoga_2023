495805c1ff902264a7b41584974eb9340e1ed8f7
mamutuberalles <teglasak@gmail.com>
2023-05-16 17:42:45 +0200

    Cleaned files for better merge

--- crypto-gaze/app/react-ui/src/Basic Components/MyChart.jsx
+++ crypto-gaze/app/react-ui/src/Basic Components/MyChart.jsx
@@ -5,7 +5,6 @@ import { Button } from "@ui5/webcomponents-react";
 import { Title } from '@ui5/webcomponents-react';
 import { useNavigate } from "react-router-dom";
 import { Card, CardHeader } from '@ui5/webcomponents-react';
-import { array, element } from "prop-types";
 
 export function MyChart(args) {
 
@@ -13,7 +12,6 @@ export function MyChart(args) {
 
     const [dataset, setDataset] = useState([]);
     const [dataFetched, setDataFetched] = useState(0);
-    const [measures,setMeasures] = useState([])
 
     const unhide = async () => {
         try {
@@ -113,69 +111,9 @@ export function MyChart(args) {
             res = await axios.get('http://localhost:4004/catalog/Crypto' + "?$filter=date ge " + args.args.start_date + " and date le " + args.args.end_date + " and ticker eq '" + args.args.ticker + "' and type eq 'real'&$orderby=date asc")
             args.args.field = "close"
         }
-/*         setDataset(res.data.value);
-        setDataFetched(dataFetched + 1); */
-
-        let arrayFull = res.data.value
-        let columnNames = ['id','date','open','high','low','close','adj_close','volume','ticker','type']
-        let zeroIndexes = []
-        columnNames.forEach(column => {
-            if(column != 'date' && column != args.args.field)
-            {
-                arrayFull.forEach(record => delete record[column])
-            }
-        });
-        
-
-        for (let index = 0; index < arrayFull.length; index++) {
-            if(arrayFull[index][args.args.field] == 0)
-            {
-                zeroIndexes.push(index)
-            }
-        }
-        console.log(zeroIndexes)
-        let array_2 = []
-        let last_place = 0
-        zeroIndexes.forEach(index => {
-            array_2.push(arrayFull.slice(last_place,index))
-            last_place = index+1
-        });
-        array_2.push(arrayFull.slice(last_place,arrayFull.length))
-
-        for (let index = 0; index < array_2.length; index++) {
-            array_2[index].forEach(element => element[args.args.field+index] = element[args.args.field])
-        }
-
-        let array3 = []
-
-        for (let index = 0; index < array_2.length; index++) {
-            array_2[index].forEach(element => array3.push(element))
-        }
-
-        let measures_temp = []
-
-        for (let index = 0; index < array_2.length; index++) {
-            measures_temp.push(
-                {
-                    accessor: args.args.field+index,
-                    label: args.args.label+index
-                }
-            )
-        }
-
-
-
-        console.log(array3)
-        setDataset(array3);
+        setDataset(res.data.value);
         setDataFetched(dataFetched + 1);
-        setMeasures(measures_temp);
     }
-
-    const removeEl = (array, remIdx) => {
-        return array.map(function(arr) {
-                return arr.filter(function(el,idx){return idx !== remIdx});  
-               });
-       };
 
     useEffect(() => {
         if (dataFetched < 1) {
@@ -186,7 +124,7 @@ export function MyChart(args) {
     return (
         <>
             <Card header={<CardHeader titleText={args.args.title} />}>
-                <LineChart measures={measures} dimensions={[{ accessor: "date", intervl:0 }]} dataset={dataset} />
+                <LineChart measures={[{ accessor: `${args.args.field}`, label: `${args.args.label}` }]} dimensions={[{ accessor: "date", intervl:0 }]} dataset={dataset} />
                 {args.args.hidden == "true"
                     ? <Button onClick={unhide}> Unhide Chart </Button>
                     : <> </>
