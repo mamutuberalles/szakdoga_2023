3f686a047d294138e6f6c93fb014a37c6f734ee0
mamutuberalles <teglasak@gmail.com>
2023-05-27 22:34:08 +0200

    Made backend tests in accordance of #73

--- crypto-gaze/srv/server.js
+++ crypto-gaze/srv/server.js
@@ -6,72 +6,79 @@ const path = require('path');
 const app = require('express')();
 
 cds.on("listening", async (app) => {
-  await DELETE.from`endpoint_model.CommandResult`
-  let CRYPTOS = await SELECT`data_model.Crypto`.groupBy('ticker')
-  for (const crypto of CRYPTOS) {
-    console.log("[INFO] Refreshing available crypto data for: " + crypto.ticker)
-    ticker = crypto.ticker
-    date = await SELECT`from data_model.Crypto where ticker = ${ticker}`.orderBy('date asc').limit('1')
-    date = date[0].date
-    console.log("[INFO] Refreshing data for: " + ticker + " from " + date)
-    if (ticker == "") {
-      console.log("[ERROR] Ticker data not given, please enter ticker data.")
-    }
-    else {
-      await DELETE.from`data_model.Crypto`.where({ ticker: ticker })
-      await DELETE.from`chart_model.PreDefinedCharts`.where({ ticker: ticker })
-      console.log("[INFO] Ticker data deleted for: " + ticker)
-      const { spawn } = require("child_process");
-      console.log("[INFO] Running script add_data.py with argument " + ticker + " " + date)
-      const pythonProcess = spawn('python', ["../python_scripts/add_data.py", ticker, date]);
-      const pythonProcess2 = spawn('python', ["../python_scripts/add_chart.py", ticker]);
-      pythonProcess.stdout.on('data', function (data) {
-        console.log("[INFO] Recieved data from add_data.py: " + data.toString())
-      });
+  if (process.env['mode'] == "test") {
+    console.log("[INFO] Testing")
+    return;
+  }
+  else if (process.env['mode'] == "run"){
+    await DELETE.from`endpoint_model.CommandResult`
+    let CRYPTOS = await SELECT`data_model.Crypto`.groupBy('ticker')
+      for(const crypto of CRYPTOS) {
+      console.log("[INFO] Refreshing available crypto data for: " + crypto.ticker)
+      ticker = crypto.ticker
+      date = await SELECT`from data_model.Crypto where ticker = ${ticker}`.orderBy('date asc').limit('1')
+      date = date[0].date
+      console.log("[INFO] Refreshing data for: " + ticker + " from " + date)
+      if (ticker == "") {
+        console.log("[ERROR] Ticker data not given, please enter ticker data.")
+      }
+      else {
+        await DELETE.from`data_model.Crypto`.where({ ticker: ticker })
+        await DELETE.from`chart_model.PreDefinedCharts`.where({ ticker: ticker })
+        console.log("[INFO] Ticker data deleted for: " + ticker)
+        const { spawn } = require("child_process");
+        console.log("[INFO] Running script add_data.py with argument " + ticker + " " + date)
+        const pythonProcess = spawn('python', ["../python_scripts/add_data.py", ticker, date]);
+        const pythonProcess2 = spawn('python', ["../python_scripts/add_chart.py", ticker]);
+        pythonProcess.stdout.on('data', function (data) {
+          console.log("[INFO] Recieved data from add_data.py: " + data.toString())
+        });
 
-      pythonProcess.on('close', async (code) => {
-        console.log("[INFO] Python process add_data.py finished with code " + code)
-      })
+        pythonProcess.on('close', async (code) => {
+          console.log("[INFO] Python process add_data.py finished with code " + code)
+        })
 
 
-      pythonProcess2.stdout.on('data', function (data) {
-        console.log("[INFO] Recieved data from add_chart.py: " + data.toString())
-      });
+        pythonProcess2.stdout.on('data', function (data) {
+          console.log("[INFO] Recieved data from add_chart.py: " + data.toString())
+        });
 
-      pythonProcess2.on('close', async (code) => {
-        console.log("[INFO] Python process add_chart.py finished with code " + code)
-        await DELETE.from`endpoint_model.CommandResult where opKey = null`
-      })
+        pythonProcess2.on('close', async (code) => {
+          console.log("[INFO] Python process add_chart.py finished with code " + code)
+          await DELETE.from`endpoint_model.CommandResult where opKey = null`
+        })
 
+      }
     }
   }
+  
 })
 
 cds.on('bootstrap', (app) => {
   app.use(cors());
   app.use("/", express.static("../react-ui/build"))
-  app.get("/experimental", (req,res) =>{
+  app.get("/experimental", (req, res) => {
     res.redirect('/')
   })
-  app.get("/charts", (req,res) =>{
+  app.get("/charts", (req, res) => {
     res.redirect('/')
   })
-  app.get("/bookmarkedcharts", (req,res) =>{
+  app.get("/bookmarkedcharts", (req, res) => {
     res.redirect('/')
   })
-  app.get("/hiddencharts", (req,res) =>{
+  app.get("/hiddencharts", (req, res) => {
     res.redirect('/')
   })
-  app.get("/addsimplechart", (req,res) =>{
+  app.get("/addsimplechart", (req, res) => {
     res.redirect('/')
   })
-  app.get("/addcomplexchart", (req,res) =>{
+  app.get("/addcomplexchart", (req, res) => {
     res.redirect('/')
   })
-  app.get("/chartmodifier", (req,res) =>{
+  app.get("/chartmodifier", (req, res) => {
     res.redirect('/')
   })
-  app.get("/scripts", (req,res) =>{
+  app.get("/scripts", (req, res) => {
     res.redirect('/')
   })
 })
@@ -80,6 +87,9 @@ cds.on('bootstrap', (app) => {
 
 
 schedule.scheduleJob('0 0 * * *', async () => {
+  if (process.env['mode'] != "run") {
+    return;
+  }
   let CRYPTOS = await SELECT`data_model.Crypto`.groupBy('ticker')
   for (const crypto of CRYPTOS) {
     console.log("[INFO] Refreshing available crypto data for: " + crypto.ticker)
--- crypto-gaze/test/test.js
+++ crypto-gaze/test/test.js
@@ -1,17 +1,176 @@
 const cds = require('@sap/cds/lib')
-const chai = require('chai');
-const request = require('supertest');
-const express = require('express');
-const app = express();
-describe('GET /', function () {
-   it('should return 200 OK and render home view', async function (done) {
-    const { data } = await GET ('/crypto/Crypto', { })
-/*       await request(app)
-          .get('/')
-          .expect(200)
-          .end(function (err, res) {
-              if (err) return done(err);
-              done();
-          }); */
-  }); 
+
+const { GET, POST, PATCH, axios, expect, DELETE } = cds.test(__dirname + '/..')
+
+describe("Testing Crypto model", () => {
+  it('POST', async () => {
+    const req = await POST(`/crypto/Crypto`, {
+      date: "2023-01-01",
+      open: 1.0,
+      high: 2.0,
+      low: 0.5,
+      close: 1.5,
+      adj_close: 1.5,
+      volume: 200,
+      ticker: "M-COIN",
+      type: "real"
+    })
+    expect(req.status).to.equal(201)
+  })
+  it('GET', async () => {
+    const req = await GET(`/crypto/Crypto?$filter=ticker eq 'M-COIN'`, {
+    })
+
+    const payload = {
+      date: "2023-01-01",
+      open: 1,
+      high: 2,
+      low: 0.5,
+      close: 1.5,
+      adj_close: 1.5,
+      volume: 200,
+      ticker: "M-COIN",
+      type: "real",
+    }
+
+    expect(req.data.value).to.deep.include(payload)
+  })
+  it('DELETE', async () => {
+    const req = await POST(`/crypto/DeleteTicker`, {
+      ticker: "M-COIN",
+      opKey: "test-opkey-1"
+    })
+    expect(req.status).to.equal(204)
+  })
+
+});
+describe("Testing PreDefinedCharts model", () => {
+  let id = null
+  it('POST', async () => {
+    const req = await POST(`/chart/PreDefinedCharts`, {
+      ticker: "M-COIN",
+      start_date: "2023-01-01",
+      end_date: "2023-05-29",
+      label: "M-COIN - USD",
+      title: "M-COIN in May"
+    })
+
+    expect(req.status).to.equal(201)
+    id = req.data.id
+  })
+  it('GET', async () => {
+    const req = await GET(`/chart/PreDefinedCharts?$filter=id eq ` + id, {
+    })
+
+    const payload = {
+      ticker: "M-COIN",
+      start_date: "2023-01-01",
+      end_date: "2023-05-29",
+      label: "M-COIN - USD",
+      title: "M-COIN in May",
+    }
+
+    const chart = req.data.value[0]
+    expect(payload['ticker']).to.equal(chart['ticker'])
+    expect(payload['start_date']).to.equal(chart['start_date'])
+    expect(payload['end_date']).to.equal(chart['end_date'])
+    expect(payload['label']).to.equal(chart['label'])
+    expect(payload['title']).to.equal(chart['title'])
+  })
+  it('DELETE', async () => {
+    const req = await DELETE(`/chart/PreDefinedCharts` + `/` + id)
+  })
+
+});
+describe("Testing CustomCharts model", () => {
+  let id = null
+  it('POST', async () => {
+    const req = await POST(`/chart/CustomCharts`, {
+      ticker: "M-COIN",
+      start_date: "2023-01-01",
+      end_date: "2023-05-29",
+      label: "M-COIN - USD",
+      title: "M-COIN in May",
+      field: "close",
+      chart_type: "complex",
+      ticker2: "V-BUX",
+      field2: "close",
+      label2: "V-BUX - USD",
+      forecast: "None",
+      bookmarked: "False",
+      hidden: "False"
+    })
+
+    expect(req.status).to.equal(201)
+    id = req.data.id
+  })
+  it('GET', async () => {
+    const req = await GET(`/chart/CustomCharts?$filter=id eq ` + id, {
+    })
+
+    const payload = {
+      ticker: "M-COIN",
+      start_date: "2023-01-01",
+      end_date: "2023-05-29",
+      label: "M-COIN - USD",
+      title: "M-COIN in May",
+      field: "close",
+      chart_type: "complex",
+      ticker2: "V-BUX",
+      field2: "close",
+      label2: "V-BUX - USD",
+      forecast: "None",
+      bookmarked: "False",
+      hidden: "False"
+    }
+
+    const chart = req.data.value[0]
+    expect(payload['ticker']).to.equal(chart['ticker'])
+    expect(payload['start_date']).to.equal(chart['start_date'])
+    expect(payload['end_date']).to.equal(chart['end_date'])
+    expect(payload['label']).to.equal(chart['label'])
+    expect(payload['title']).to.equal(chart['title'])
+    expect(payload['field']).to.equal(chart['field'])
+    expect(payload['chart_type']).to.equal(chart['chart_type'])
+    expect(payload['ticker2']).to.equal(chart['ticker2'])
+    expect(payload['forecast']).to.equal(chart['forecast'])
+    expect(payload['bookmarked']).to.equal(chart['bookmarked'])
+    expect(payload['hidden']).to.equal(chart['hidden'])
+  })
+  it('DELETE', async () => {
+    const req = await DELETE(`/chart/CustomCharts` + `/` + id)
+  })
+
 }); 
+
+describe("Testing CommandResult model", () => {
+  let id = null
+  it('POST', async () => {
+    const req = await POST(`/endpoint/CommandResult`, {
+      command: "Test command",
+      data : "Test data",
+      opKey : "test-opkey"
+    })
+    expect(req.status).to.equal(201)
+    id = "test-opkey"
+  })
+  it('GET', async () => {
+    const req = await GET(`/endpoint/CommandResult?$filter=opKey eq '`+id+`'`, {
+    })
+
+    const payload = {
+      command: "Test command",
+      data : "Test data",
+      opKey : "test-opkey"
+    }
+
+    expect(req.data.value).to.deep.include(payload)
+  })
+  it('DELETE', async () => {
+    const req = await POST(`/endpoint/DeleteResult`, {
+      opKey: "test-opkey"
+    })
+    expect(req.status).to.equal(204)
+  })
+
+});
